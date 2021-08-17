eb747754e14234827777a4f2ebad2b84a37c3c45
fkf <1475794025@qq.com>
2021-08-17 16:12:29 +0800

    C2客户端更新操作1

--- src/test/java/org/example/Test02.java
+++ src/test/java/org/example/Test02.java
@@ -7,5 +7,6 @@ package org.example;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("Test02");
+        System.out.println("C2客户端操作->Test02");
     }
 }
