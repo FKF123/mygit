41c19718ba78c0b4ea46767bd1861882475c80a8
fkf <1475794025@qq.com>
2021-08-17 14:47:14 +0800

    第一次提交

---
+++ src/test/java/org/example/Test01.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author shkstart
+ * @create 2021-08-17-14:33
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("test01");
+    }
+}
