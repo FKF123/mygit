535b2ec20345a92168e4af27ee680aa0cca22cb4
fkf <1475794025@qq.com>
2021-08-17 15:03:14 +0800

    第二次提交

--- src/test/java/org/example/Test01.java
+++ src/test/java/org/example/Test01.java
@@ -7,5 +7,8 @@ package org.example;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("test01");
+        System.out.println("test01");
+        System.out.println("test01");
+        System.out.println("test01");
     }
 }
