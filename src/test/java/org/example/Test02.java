d7066638116ea4ae3fccf498a6da049901ed901b
fkf <1475794025@qq.com>
2021-08-17 15:36:25 +0800

    dev分支添加test02.java

---
+++ src/test/java/org/example/Test02.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author shkstart
+ * @create 2021-08-17-15:35
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("Test02");
+    }
+}
